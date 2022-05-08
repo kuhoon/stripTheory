5c8048d06f54888124c7ac1a8aa8cddf092b245a
kuhoo <kuhoon.kwon@rwth-aachen.de>
2022-05-08 04:20:17 +0200

    change spline2 to spline4. clear.

--- fileManager145_caero4.py
+++ fileManager145_caero4.py
@@ -243,9 +243,6 @@ model.add_aelist(1, toti)
 
 # insert model.add_spline
 model.add_spline4(int(1), 105001, int(1), int(1), float(), 'FPS', 'BOTH', int(10), int(10)) #Still considering which spline is suitable
-# model.add_spline2(int(1), int(103001), int(103001), int(103005), int(1), float(0.), float(1.), int(0), float(0.), float(0.), 'BOTH')
-# model.add_spline2(int(2), int(104001), int(104001), int(104014), int(1), float(0.), float(1.), int(0), float(0.), float(0.), 'BOTH')
-# model.add_spline2(int(3), int(105001), int(105001), int(105067), int(1), float(0.), float(1.), int(0), float(0.), float(0.), 'BOTH')
 
 # manage flfact
 seaAD = 1.225E-12
